b73d65581008485dad0697370dba6baabe6551ce
Shahar Yair <shaharyair12@gmail.com>
2023-11-16 23:30:14 +0200

    refactor: Move functions to parent product component
    For better props usage i have moved all the product functions to product main component to be able to pass props to every component in product main.

--- app/components/ProductList.js
+++ app/components/ProductList.js
@@ -1,6 +1,3 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-
 import Image from "next/image";
 
 import { CircularProgress, IconButton } from "@mui/material";
@@ -8,65 +5,15 @@ import { Delete } from "@mui/icons-material";
 
 import noImage from "../assests/noImage.jpg";
 
-export default function ProductList({ setProductById }) {
-  const [productsList, setProductsList] = useState();
-  const [loading, setLoading] = useState(false);
-
-  const handleDeleteProduct = (productId, e) => {
-    e.stopPropagation();
-    setLoading(true);
-
-    axios
-      .delete(`http://localhost:3000/api/products/deleteProduct/${productId}`)
-      .then((response) => {
-        fetchProductList();
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error(`Error deleting product with id ${productId}:`, error);
-        setLoading(false);
-      });
-  };
-
-  const fetchProductById = (productId) => {
-    axios
-      .get(`http://localhost:3000/api/products/${productId}`)
-      .then((response) => {
-        setProductById(response.data.productById);
-      })
-      .catch((error) => {
-        console.error(`Error fetching product with id ${productId}:`, error);
-        setLoading(false);
-      });
-  };
-
-  const fetchProductList = () => {
-    setLoading(true);
-
-    axios
-      .get("http://localhost:3000/api/products")
-      .then((response) => {
-        setProductsList(response.data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error fetching products:", error);
-        setLoading(false);
-      });
-  };
-
-  useEffect(() => {
-    fetchProductList();
-  }, []);
-
+export default function ProductList({ loading, productList, fetchProductById, handleDeleteProduct }) {
   return (
     <>
       {loading ? (
         <CircularProgress size={100} />
       ) : (
         <main className='flex justify-center flex-col gap-2 text-left w-full'>
-          {productsList &&
-            productsList.products.map((product) => (
+          {productList &&
+            productList.products.map((product) => (
               <div
                 key={`Product id: ${product._id}`}
                 className='flex flex-col md:flex-row justify-between items-center border-[2px] border-black min-h-[50px] p-2.5 gap-4 bg-white drop-shadow-md cursor-pointer transition-colors hover:bg-blue-100'
@@ -74,9 +21,13 @@ export default function ProductList({ setProductById }) {
               >
                 <Image src={noImage} alt={noImage} className=' w-24 h-24 drop-shadow-sm border-[1px]' />
                 <div className='mr-auto'>
-                  <h1 className='font-semibold md:text-lg'>{product.Name !== undefined && product.Name !== "" ? product.Name : "No Name"}</h1>
+                  <h1 className='font-semibold md:text-lg'>
+                    {product.Name !== undefined && product.Name !== "" ? product.Name : "No Name"}
+                  </h1>
                   <p className='font-thin text-sm md:text-base'>
-                    {product.Description !== undefined && product.Description !== "" ? product.Description : "No Description"}
+                    {product.Description !== undefined && product.Description !== ""
+                      ? product.Description
+                      : "No Description"}
                   </p>
                 </div>
                 <IconButton aria-label='delete' onClick={(e) => handleDeleteProduct(product._id, e)}>
